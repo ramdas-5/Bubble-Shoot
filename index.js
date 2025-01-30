ad846cf9e156464539357adeda6785770ff4ad88
ramdas-5 <ramdashembram05@gmail.com>
2025-01-30 23:34:53 +0530

    now good

--- index.js
+++ index.js
@@ -1,8 +1,28 @@
 var hitrn = 0;
 var timer = 60;
 var score = 0;
+var timerint;
 
-function makeBubble(){
+function startGame() {
+    // Hide the start screen
+    document.getElementById('gameStartScreen').style.display = 'none';
+
+    // Show the game panel
+    document.querySelector('.main').style.display = 'flex';
+
+    // Initialize the game
+    resetGame();
+    runTimer();
+    makeBubble();
+}
+
+function restartGame() {
+    // Reset the game state
+    document.getElementById('gameOverScreen').style.display = 'none';
+    startGame();
+}
+
+function makeBubble() {
     var clutter = "";
     for (var i = 1; i <= 150; i++) {
         var random = Math.floor(Math.random() * 10);
@@ -12,37 +32,44 @@ function makeBubble(){
 }
 
 function runTimer() {
-    var timerint = setInterval(function () {
+    timerint = setInterval(function () {
         if (timer > 0) {
             timer--;
             document.querySelector("#timerval").innerHTML = timer;
         } else {
             clearInterval(timerint);
-            document.querySelector("#finalScore").innerHTML = score; 
-            document.querySelector("#gameOverScreen").style.display = "block"; 
-            document.querySelector(".panel").style.display = "none"; 
+            document.querySelector("#finalScore").innerHTML = score;
+            document.querySelector("#gameOverScreen").style.display = "block";
+            document.querySelector(".main").style.display = "none";
         }
     }, 1000);
 }
-function getNewHit(){
+
+function getNewHit() {
     hitrn = Math.floor(Math.random() * 10);
     document.querySelector("#hitval").innerHTML = hitrn;
 }
 
-function increaseScore(){
+function increaseScore() {
     score += 10;
     document.querySelector("#scoreval").innerHTML = score;
 }
 
-document.querySelector(".pbtm").addEventListener("click", function(dets){
+function resetGame() {
+    hitrn = 0;
+    timer = 60;
+    score = 0;
+    document.querySelector("#scoreval").innerHTML = score;
+    document.querySelector("#timerval").innerHTML = timer;
+    getNewHit();
+    makeBubble();
+}
+
+document.querySelector(".pbtm").addEventListener("click", function (dets) {
     var clickednum = Number(dets.target.innerHTML);
-    if(clickednum === hitrn){
+    if (clickednum === hitrn) {
         increaseScore();
         makeBubble();
         getNewHit();
     }
 });
-
-getNewHit();
-runTimer();
-makeBubble();
